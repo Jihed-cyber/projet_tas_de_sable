0f62d8122ded8793051ee9af9ad97a420d00815b
Jihed redjil <jihed.redjil@uvsq.fr>
2022-02-14 16:50:00 +0100

    modif

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,7 +1,10 @@
 ###################################################
-# groupe LDDMP 3
+# groupe LDDMP 
 # Jihed Redjil
 # Ryan Achibet
 # Jean Le Mire
 #https://github.com/Jihed-cyber/projet_tas_de_sable
 ####################################################
+
+#import des librairies#
+import tkinter as tk()
